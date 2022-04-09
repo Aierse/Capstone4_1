0a688e0e5b7406c65b66b3f36a3396f2ee222df5
Aierse <dbsgur2008@gmail.com>
2022-04-09 17:09:23 +0900

    리팩토링 - 캐릭터 생성창 뷰 바인딩 적용

--- app/src/main/java/com/example/capstone4_1/CreateCharacterActivity.kt
+++ app/src/main/java/com/example/capstone4_1/CreateCharacterActivity.kt
@@ -2,10 +2,14 @@ package com.example.capstone4_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.capstone4_1.databinding.ActivityCreateCharacterBinding
 
 class CreateCharacterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateCharacterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_character)
+        binding = ActivityCreateCharacterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
