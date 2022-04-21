16e753a76a2113e24593879ce4763b7d074e837b
Aierse <dbsgur2008@gmail.com>
2022-04-22 01:08:19 +0900

    기능 - 싱글턴 적용

--- app/src/main/java/com/example/capstone4_1/Character.kt
+++ app/src/main/java/com/example/capstone4_1/Character.kt
@@ -1,18 +1,13 @@
 package com.example.capstone4_1
 
-class Character {
-    val name: String
-    get() {
-        return name
-    }
+object Character {
+    lateinit var name: String
+    lateinit var gender: String
+    lateinit var interest: String
 
-    val gender: String
-    get() {
-        return gender
-    }
-
-    val interest: String
-    get() {
-        return interest
+    fun initialize(name: String, gender: String, interest: String) {
+        this.name = name
+        this.gender = gender
+        this.interest = interest
     }
 }
