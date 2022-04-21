6e3b8f9060d14b3d55ae6e322aabd8a85d3dce2e
Aierse <dbsgur2008@gmail.com>
2022-04-22 00:39:04 +0900

    추가 - 캐릭터 클래스

---
+++ app/src/main/java/com/example/capstone4_1/Character.kt
@@ -0,0 +1,18 @@
+package com.example.capstone4_1
+
+class Character {
+    val name: String
+    get() {
+        return name
+    }
+
+    val gender: String
+    get() {
+        return gender
+    }
+
+    val interest: String
+    get() {
+        return interest
+    }
+}
