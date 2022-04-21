1730eff6b79f0dedff3bbd531495a1b9f6a35a95
Aierse <dbsgur2008@gmail.com>
2022-04-22 01:21:22 +0900

    수정 - 관심분야 class로 적용

--- app/src/main/java/com/example/capstone4_1/Character.kt
+++ app/src/main/java/com/example/capstone4_1/Character.kt
@@ -3,9 +3,9 @@ package com.example.capstone4_1
 object Character {
     lateinit var name: String
     lateinit var gender: String
-    lateinit var interest: String
+    lateinit var interest: Interest
 
-    fun initialize(name: String, gender: String, interest: String) {
+    fun initialize(name: String, gender: String, interest: Interest) {
         this.name = name
         this.gender = gender
         this.interest = interest
