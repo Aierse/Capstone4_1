52eedd146d87f705b16943edfea1db59f308e734
Aierse <dbsgur2008@gmail.com>
2022-04-22 14:46:28 +0900

    리팩토링 - 기본값 적용 위치 변경

--- app/src/main/java/com/example/capstone4_1/MainActivity.kt
+++ app/src/main/java/com/example/capstone4_1/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setFrag(1)
         binding.btnFrag2.setOnClickListener {
             setFrag(1)
         }
