c8dcbea369d092e997027cd6d398fab77831c810
khj5014 <gmlwo5011@naver.com>
2022-04-24 00:35:57 +0900

    기능 - 프레그먼트 적용

--- app/src/main/java/com/example/capstone4_1/MainActivity.kt
+++ app/src/main/java/com/example/capstone4_1/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.capstone4_1
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -36,22 +34,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupEvent() {
-        binding.mainList.onItemClickListener =
-            AdapterView.OnItemClickListener { parent, view, position, id ->
-                val dialog = AlertDialog.Builder(this@MainActivity)
-
-                val selectQuest = parent.getItemAtPosition(position) as Quest
-                dialog.setTitle(selectQuest.name)
-                dialog.setMessage(selectQuest.explain)
-
-                dialog.setPositiveButton("확인") { dialogInterface, i ->
-                    toast("test")
-                }
-
-                dialog.setCancelable(false)
-                dialog.show()
-            }
-
         binding.btnFrag1.setOnClickListener { setFrag(CallFragment.MYINFO) }
         binding.btnFrag2.setOnClickListener { setFrag(CallFragment.QUSETSCREEN) }
     }
@@ -73,12 +55,12 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         val fm = supportFragmentManager
 
-        if (binding.mainList.visibility == View.VISIBLE) {
+        if (binding.mainFrag.visibility == View.VISIBLE) {
             super.onBackPressed()
         }
         //동작 테스트 코드
         fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
-        binding.mainList.setVisibility(View.VISIBLE)
+        binding.mainFrag.setVisibility(View.VISIBLE)
     }
 
     //감지된 화면 번호 받아서 프래그먼트 매니저 동작 처리하는 곳
@@ -89,15 +71,12 @@ class MainActivity : AppCompatActivity() {
         when (fragnum) {
             CallFragment.MAIN -> {
                 ft.replace(R.id.mainFrag, MainScreenFragment()).commit()
-                binding.mainList.setVisibility(View.VISIBLE)
             }
             CallFragment.MYINFO -> {
                 ft.replace(R.id.mainFrag, MyInfoFragment()).addToBackStack(null).commit()
-                binding.mainList.setVisibility(View.GONE)
             }
             CallFragment.QUSETSCREEN -> {
                 ft.replace(R.id.mainFrag, QuestScreenFragment()).addToBackStack(null).commit()
-                binding.mainList.setVisibility(View.GONE)
             }
         }
     }
@@ -116,7 +95,7 @@ class MainScreenFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(activity_main_screen, container, false)
 
-        val rootView = view.findViewById<ListView>(R.id.mainList)
+        val rootView = view.findViewById<ListView>(R.id.mainFrag)
 
         return view
     }
@@ -133,6 +112,23 @@ class MyInfoFragment : Fragment() {
     }
 }
 
+class QuestListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(quest_list, container, false)
+
+        val rootView = view.findViewById<ListView>(R.id.questlist)
+        val adapter = QuestAdapter(requireContext())
+
+        rootView.adapter = adapter
+
+        return view
+    }
+}
+
 class QuestScreenFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -140,12 +136,6 @@ class QuestScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(activity_quest_screen, container, false)
-
-        val rootView = view.findViewById<ListView>(R.id.questlist)
-        val adapter = QuestAdapter(requireContext())
-
-        rootView.adapter = adapter
-
         return view
     }
 }
