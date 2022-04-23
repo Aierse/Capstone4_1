1e629837819b726ba2e6b0576cc3ab0f91deaa55
khj5014 <gmlwo5011@naver.com>
2022-04-23 09:05:15 +0900

    수정 - 동작 감지 및 뒤로가기추가

--- app/src/main/java/com/example/capstone4_1/MainActivity.kt
+++ app/src/main/java/com/example/capstone4_1/MainActivity.kt
@@ -9,8 +9,7 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.capstone4_1.R.layout.activity_my_info
-import com.example.capstone4_1.R.layout.activity_quest_screen
+import com.example.capstone4_1.R.layout.*
 import com.example.capstone4_1.databinding.ActivityMainBinding
 
 
@@ -22,10 +21,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //최초실행 프래그먼트(앱 실행시 출력되는 화면)
+        setFrag(0)
+
+        //퀘스트 읽기 리스트 동작
         val Adapter = QuestAdapter(this, Character.QuestList)
         val listview = findViewById<View>(R.id.mainList) as ListView
         listview.adapter = Adapter
 
+
+        //퀘스트 선택 감지 동작 (현재는 퀘스트 이름이 출력이 되는 코드)
         listview.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id ->
 
@@ -33,20 +38,19 @@ class MainActivity : AppCompatActivity() {
                 selectItem.Qname
                 toast(selectItem.Qname)
             }
-        binding.btnFrag1.setOnClickListener {
-            setFrag(0)
-            binding.mainList.setVisibility(View.INVISIBLE)
-        }
+
+        //메뉴버튼 클릭 감지
+        binding.btnFrag1.setOnClickListener { setFrag(1) }
         binding.btnFrag2.setOnClickListener {
-            binding.mainList.setVisibility(View.VISIBLE)
-            setFrag(1)
+            setFrag(2)
             /* 퀘스트페이지  화면 전환으로 실행 코드 (필요시 활성화 예정)
             val intent = Intent(this, QuestScreen::class.java)
             startActivity(intent)*/
         }
+
     }
 
-    //토스트 메시지 사용법 -->  기본값 String toast("내용")  형변환 예시 --> 변수.toString()
+    //토스트 메시지 사용법 -->  기본값 String toast("내용")  형변환 예시 --> toast(변수.toString())
     private fun toast(message: String) {
         var toast: Toast? = null
         // 토스트 메서드
@@ -56,20 +60,37 @@ class MainActivity : AppCompatActivity() {
         toast?.show()
     }
 
+    //감지된 화면 번호 받아서 프래그먼트 매니저로 처리하는 곳
     private fun setFrag(fragnum: Int) {
 
         val ft = supportFragmentManager.beginTransaction()
 
         when (fragnum) {
             0 -> {
-                ft.replace(R.id.mainFrag, MyInfoFragment()).commit()
+                ft.replace(R.id.mainFrag, MainScreenFragment()).addToBackStack(null).commit()
+                binding.mainList.setVisibility(View.VISIBLE)
             }
             1 -> {
-                ft.replace(R.id.mainFrag, QuestScreenFragment()).commit()
+                ft.replace(R.id.mainFrag, MyInfoFragment()).addToBackStack(null).commit()
+                binding.mainList.setVisibility(View.GONE)
+            }
+            2 -> {
+                ft.replace(R.id.mainFrag, QuestScreenFragment()).addToBackStack(null).commit()
+                binding.mainList.setVisibility(View.GONE)
             }
         }
+    }
 
-
+    //프래그먼트별 동작
+    class MainScreenFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val view = inflater.inflate(activity_main_screen, container, false)
+            return view
+        }
     }
 
     class MyInfoFragment : Fragment() {
@@ -93,6 +114,7 @@ class MainActivity : AppCompatActivity() {
             return view
         }
     }
+
 }
 
 
