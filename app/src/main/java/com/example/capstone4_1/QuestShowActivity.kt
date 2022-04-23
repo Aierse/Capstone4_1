efb4a11919ad05d01d1bf7d64f9941dfc310c8d1
khj5014 <gmlwo5011@naver.com>
2022-04-23 09:05:46 +0900

    생성 - 퀘스트 쇼 화면 클래스

---
+++ app/src/main/java/com/example/capstone4_1/QuestShowActivity.kt
@@ -0,0 +1,13 @@
+package com.example.capstone4_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QuestShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quest_show_popup)
+
+
+    }
+}
