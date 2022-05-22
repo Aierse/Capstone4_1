7d657a5fa68ecefdcc7cd62ee3ab26d6626fdbee
Aierse <dbsgur2008@gmail.com>
2022-05-22 16:33:31 +0900

    기능 - 뷰페이저 적용

--- app/src/main/java/com/example/capstone4_1/CreateCharacterActivity.kt
+++ app/src/main/java/com/example/capstone4_1/CreateCharacterActivity.kt
@@ -2,7 +2,13 @@ package com.example.capstone4_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
 import com.example.capstone4_1.databinding.ActivityCreateCharacterBinding
 
 class CreateCharacterActivity : AppCompatActivity() {
@@ -11,10 +17,38 @@ class CreateCharacterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateCharacterBinding.inflate(layoutInflater)
+
+        binding.viewPager.adapter = CharacterListPagerAdapter(arrayListOf(R.drawable.sprite_char1, R.drawable.sprite_char2, R.drawable.sprite_char3))
+
         setContentView(binding.root)
     }
+}
 
-    fun leftMove(view: View) {
+class CharacterListPagerAdapter(private val data: ArrayList<Int>) : PagerAdapter() {
+    inner class ViewHolder {
+        lateinit var image: ImageView
+    }
 
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val inflater = LayoutInflater.from(container.context)
+        val view = inflater.inflate(R.layout.character_list, container, false)
+
+        return view.apply {
+            this.findViewById<ImageView>(R.id.imageCharacter).setImageResource(data[position])
+
+            container.addView(this)
+        }
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, obj: Any) {
+        container.removeView(obj as View?)
+    }
+
+    override fun isViewFromObject(view: View, obj: Any): Boolean {
+        return view == obj
+    }
+
+    override fun getCount(): Int {
+        return data.size
     }
 }
