2d8de313f298ff1ffeee58d3191f4776b6a8315f
Aierse <dbsgur2008@gmail.com>
2022-05-22 16:34:03 +0900

    정리

--- app/src/main/java/com/example/capstone4_1/CreateCharacterActivity.kt
+++ app/src/main/java/com/example/capstone4_1/CreateCharacterActivity.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.viewpager.widget.PagerAdapter
 import com.example.capstone4_1.databinding.ActivityCreateCharacterBinding
 
