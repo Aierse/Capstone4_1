934c3db99d3e6141a0709acb0ea74a1c0229d3b2
seosunwon <93324626+seosunwon@users.noreply.github.com>
2022-05-31 22:39:55 +0900

    1

---
+++ app/src/main/java/com/example/capstone4_1/quest_custom.kt
@@ -0,0 +1,11 @@
+package com.example.capstone4_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quest_custom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quest_custom)
+    }
+}
