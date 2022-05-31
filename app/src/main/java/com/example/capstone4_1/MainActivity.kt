3d19baaaeb0860612260d5962c42958a3d535699
khj5014 <gmlwo5011@naver.com>
2022-05-31 21:49:56 +0900

    추가 - 기능 강제 종료 시 저장

--- app/src/main/java/com/example/capstone4_1/MainActivity.kt
+++ app/src/main/java/com/example/capstone4_1/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.capstone4_1
 
+import android.app.Service
 import android.content.Intent
 import android.os.Bundle
+import android.os.IBinder
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.capstone4_1.databinding.ActivityMainBinding
@@ -12,41 +15,35 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import java.io.File
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val RESPONSE_CREATE_CHARACTER = 50
-
     var myInfoFragment: Fragment = MyinfoFragment()
+
     var questScreenFragment: Fragment = QuestListFragment()
     var homeFragment: Fragment = HomeFragment()
-
     var fragmentManager = supportFragmentManager
-
 
     // 최초 실행 시 초기화 함수
     private fun initialize() {
         // 캐릭터 클래스 초기화
-        val filepath= filesDir.toString() + "/data.json"
-        val file =File(filepath)
-        if(file.exists()){ // 파일이 존재 할경우
+        val filepath = filesDir.toString() + "/data.json"
+        val file = File(filepath)
+        if (file.exists()) { // 파일이 존재 할경우
             Character.loadCharacter(this)
-
-        }
-        else{ // 아닐경우
-            Character.initializeQuest()
+            findViewById<BottomNavigationView>(R.id.menu_bottom_navigation).selectedItemId =
+                R.id.home
+        } else { // 아닐경우
+            excuteCreateCharacterActivity()
         }
     }
-//사용할 때 주석제거 후 사용
-//    private fun setupEvent() { }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        startService(Intent(this, AutoSave::class.java))
 
-
-        initialize()
         // setupEvent()
 
         //프래그먼트트랜잭션
@@ -67,8 +64,8 @@ class MainActivity : AppCompatActivity() {
 
             true
         })
-        // 파일 검사 해야함
-        excuteCreateCharacterActivity()
+
+        initialize()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -76,7 +73,8 @@ class MainActivity : AppCompatActivity() {
 
         when (requestCode) {
             RESPONSE_CREATE_CHARACTER -> {
-                findViewById<BottomNavigationView>(R.id.menu_bottom_navigation).selectedItemId = R.id.home
+                findViewById<BottomNavigationView>(R.id.menu_bottom_navigation).selectedItemId =
+                    R.id.home
             }
         }
     }
@@ -94,5 +92,17 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         Character.saveCharacter(this)
         super.onDestroy()
+    }
+
+}
+
+class AutoSave : Service() {
+    @Nullable
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent) { //핸들링 하는 부분
+        Character.saveCharacter(this)
     }
 }
