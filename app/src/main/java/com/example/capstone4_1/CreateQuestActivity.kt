4eabcf870a383a67d1332f56f15fcc978d8a5440
khj5014 <gmlwo5011@naver.com>
2022-06-01 06:24:32 +0900

    추가 - 퀘스트 생성 액티비티

---
+++ app/src/main/java/com/example/capstone4_1/CreateQuestActivity.kt
@@ -0,0 +1,36 @@
+package com.example.capstone4_1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class CreateQuestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_quest)
+        val backbtn: Button = findViewById(R.id.backbtn)
+        val makebtn: Button = findViewById(R.id.Makebtn)
+        val qname = findViewById<EditText>(R.id.qname)
+        val qexplain = findViewById<EditText>(R.id.qexplain)
+        val questList = findViewById<ListView>(R.id.questListView)
+
+        makebtn.setOnClickListener() {
+            Character.questList.add(Quest(R.drawable.ball,"3","sad"))
+
+//            questList.adapter = QuestAdapter(this)
+            QuestAdapter(this).notifyDataSetChanged()
+
+        }
+
+        backbtn.setOnClickListener() {
+            Character.saveCharacter(this)
+            finish()
+        }
+
+    }
+
+}
