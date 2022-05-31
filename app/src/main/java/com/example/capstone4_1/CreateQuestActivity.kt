73720ce2721da67577cba9494b75a42e82f32f68
khj5014 <gmlwo5011@naver.com>
2022-06-01 07:06:44 +0900

    수정 - 퀘스트 생성 갱신

--- app/src/main/java/com/example/capstone4_1/CreateQuestActivity.kt
+++ app/src/main/java/com/example/capstone4_1/CreateQuestActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class CreateQuestActivity : AppCompatActivity() {
@@ -15,21 +16,26 @@ class CreateQuestActivity : AppCompatActivity() {
         val makebtn: Button = findViewById(R.id.Makebtn)
         val qname = findViewById<EditText>(R.id.qname)
         val qexplain = findViewById<EditText>(R.id.qexplain)
-//        val questList = findViewById<ListView>(R.id.questListView)
+//        val questList = view.findViewById<ListView>(R.id.questListView)
+        val questAdapter= QuestAdapter(this)
 
-        makebtn.setOnClickListener() {
-            Character.questList.add(Quest(R.drawable.ball,qname.text.toString(),qexplain.text.toString()))
+        makebtn.setOnClickListener() { view ->
+            Character.questList.add(Quest(R.drawable.ball,qname.text.toString(),qexplain.text.toString(),7))
 
 //            questList.adapter = QuestAdapter(this)
-            QuestAdapter(this).notifyDataSetChanged()
-
+            questAdapter.notifyDataSetChanged()
         }
 
         backbtn.setOnClickListener() {
             Character.saveCharacter(this)
+            findViewById<BottomNavigationView>(R.id.menu_bottom_navigation).selectedItemId = R.id.home
+            findViewById<BottomNavigationView>(R.id.menu_bottom_navigation).selectedItemId = R.id.questList
             finish()
         }
 
     }
 
+    override fun onTopResumedActivityChanged(isTopResumedActivity: Boolean) {
+        super.onTopResumedActivityChanged(isTopResumedActivity)
+    }
 }
