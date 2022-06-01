73099b84b2f92a705fb18af6117d726f3a50f728
ChoiXD <jongkzz@naver.com>
2022-06-02 00:32:28 +0900

    기능-통계출력

--- app/src/main/java/com/example/capstone4_1/Statistics.kt
+++ app/src/main/java/com/example/capstone4_1/Statistics.kt
@@ -1,12 +1,57 @@
 package com.example.capstone4_1
 
-data class Statistics (val genre: String, val title: String, val count: Int, val limitCount: String, val tier: String){
+import android.util.Log
+
+val healthTitle : Array<String> = arrayOf("팔굽혀 펴기", "풀업" , "뜀걸음")
+val studyTitle : Array<String> = arrayOf("영단어 외우기", "단어의 뜻 말하기" , "독서")
+val codingTitle : Array<String> = arrayOf("백준 문제 풀기", "코딩 영상 보기", "코딩 실습 ")
+val tierList: Array<String> = arrayOf("bronze_tier", "silver_tier", "gold_tier")
+val limitList: Array<Int> = arrayOf(100,1000,10000)
+
+//<a href="https://www.flaticon.com/kr/free-icons/" title="헬스장 아이콘">헬스장 아이콘  제작자: Payungkead - Flaticon</a>
+data class Statistics(val count: Int ){
+    val genre: String = Character.interest.toString().lowercase()
+    var title: Array<String> = initializeTitle()
+    var limit: Int = initializeLimit()
+    var tier: String = initializeTier()
+
+
+    fun initializeTitle(): Array<String> {
+        val i = this.genre
+        return if (i == "health") {
+            healthTitle
+        } else if (i == "study") {
+            studyTitle
+        } else {
+            codingTitle
+        }
+    }
+
+    fun initializeLimit(): Int {
+        return if (count <= limitList[0]) {
+            100
+        } else if (limitList[0] < count || count <= limitList[1]) {
+            1000
+        } else {
+            10000
+        }
+    }
+
+    fun initializeTier(): String {
+        return if (count <= limitList[0]) {
+            tierList[0]
+        } else if (limitList[0] < count || count <= limitList[1]) {
+            tierList[1]
+        } else {
+            tierList[2]
+        }
+    }
+
     companion object{
         var statisticsList = arrayListOf<Statistics>(
-            Statistics("ball","팔굽혀 펴기",500,"/1000","silver_tier"),
-            Statistics("ball", "풀업" ,5000,"/10000","gold_tier"),
-            Statistics("ball", "뜀걸음" ,10,"/100","bronze_tier")
-
+            Statistics(200)
+            ,Statistics(1000)
+            ,Statistics(50)
         )
     }
 }
