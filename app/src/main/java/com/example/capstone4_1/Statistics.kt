917eb8ba691995452814ead2bf82201ffe0e9e6b
ChoiXD <jongkzz@naver.com>
2022-06-02 23:36:59 +0900

    픽스

--- app/src/main/java/com/example/capstone4_1/Statistics.kt
+++ app/src/main/java/com/example/capstone4_1/Statistics.kt
@@ -1,7 +1,5 @@
 package com.example.capstone4_1
 
-import android.util.Log
-
 val healthTitle : Array<String> = arrayOf("팔굽혀 펴기", "풀업" , "뜀걸음")
 val studyTitle : Array<String> = arrayOf("영단어 외우기", "단어의 뜻 말하기" , "독서")
 val codingTitle : Array<String> = arrayOf("백준 문제 풀기", "코딩 영상 보기", "코딩 실습 ")
@@ -9,12 +7,11 @@ val tierList: Array<String> = arrayOf("bronze_tier", "silver_tier", "gold_tier")
 val limitList: Array<Int> = arrayOf(100,1000,10000)
 
 //<a href="https://www.flaticon.com/kr/free-icons/" title="헬스장 아이콘">헬스장 아이콘  제작자: Payungkead - Flaticon</a>
-data class Statistics(val count: Int ){
+data class Statistics(val count: Int ) {
     val genre: String = Character.interest.toString().lowercase()
     var title: Array<String> = initializeTitle()
     var limit: Int = initializeLimit()
     var tier: String = initializeTier()
-
 
     fun initializeTitle(): Array<String> {
         val i = this.genre
@@ -30,7 +27,7 @@ data class Statistics(val count: Int ){
     fun initializeLimit(): Int {
         return if (count <= limitList[0]) {
             100
-        } else if (limitList[0] < count || count <= limitList[1]) {
+        } else if (limitList[0] < count && count <= limitList[1]) {
             1000
         } else {
             10000
@@ -40,12 +37,14 @@ data class Statistics(val count: Int ){
     fun initializeTier(): String {
         return if (count <= limitList[0]) {
             tierList[0]
-        } else if (limitList[0] < count || count <= limitList[1]) {
+        } else if (limitList[0] < count && count <= limitList[1]) {
             tierList[1]
         } else {
             tierList[2]
         }
     }
+
+
 
     companion object{
         var statisticsList = arrayListOf<Statistics>(
