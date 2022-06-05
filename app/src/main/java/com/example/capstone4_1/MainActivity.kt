a74461e19680a1edfe717edf39c75314ff31af21
khj5014 <gmlwo5011@naver.com>
2022-06-06 05:05:00 +0900

    정리

--- app/src/main/java/com/example/capstone4_1/MainActivity.kt
+++ app/src/main/java/com/example/capstone4_1/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.IBinder
 import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.capstone4_1.databinding.ActivityMainBinding
 import com.example.capstone4_1.fragment.HomeFragment
 import com.example.capstone4_1.fragment.MyinfoFragment
@@ -22,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     var questScreenFragment: Fragment = QuestListFragment()
     var homeFragment: Fragment = HomeFragment()
-    var fragmentManager = supportFragmentManager
+    var FM = supportFragmentManager
 
     // 최초 실행 시 초기화 함수
     private fun initialize() {
@@ -32,7 +34,8 @@ class MainActivity : AppCompatActivity() {
 
         if (file.exists()) { // 파일이 존재 할경우
             Character.loadCharacter(this)
-            findViewById<BottomNavigationView>(R.id.menu_bottom_navigation).selectedItemId = R.id.home
+            findViewById<BottomNavigationView>(R.id.menu_bottom_navigation).selectedItemId =
+                R.id.home
         } else { // 아닐경우
             excuteCreateCharacterActivity()
         }
@@ -51,19 +54,38 @@ class MainActivity : AppCompatActivity() {
 
         //프래그먼트 메뉴
         bottomNavigation.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
-            val transaction = fragmentManager.beginTransaction()
+            val transaction = FM.beginTransaction()
 
             when (item.itemId) {
                 R.id.myInfo -> transaction.replace(R.id.mainFrag, myInfoFragment).commit()
                 R.id.home -> transaction.replace(R.id.mainFrag, homeFragment).commit()
                 R.id.questList -> transaction.replace(R.id.mainFrag, questScreenFragment).commit()
             }
-
             true
         })
-
         initialize()
+
+    //        finishQuest.setText(Character.doingQuetstCount.toString() + " / 3")
+//        thread(start = true) {
+//            var i = 0
+//
+//            while(i < 10) {
+//                i += 1
+//
+//                runOnUiThread {    //Ui에 접근할 수 있음
+//                    finishQuest.text = "카운트 : ${i}"
+//                }
+//
+//                Thread.sleep(1000)	//1000 == 1초
+//            }
+//        }
     }
+
+    fun refreshFragment(fragment: Fragment, FM: FragmentManager) {
+        val ft: FragmentTransaction = FM.beginTransaction()
+        ft.detach(fragment).attach(fragment).commit()
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -80,11 +102,13 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         finish()
     }
+
     //내 정보 생성
     private fun excuteCreateCharacterActivity() {
         val intent = Intent(this, CreateCharacterActivity::class.java)
         startActivityForResult(intent, RESPONSE_CREATE_CHARACTER)
     }
+
     override fun onDestroy() {
         Character.saveCharacter(this)
         super.onDestroy()
