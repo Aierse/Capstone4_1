89b1d489fa3847b19703d1c0b86a37234da145eb
khj5014 <gmlwo5011@naver.com>
2022-06-07 22:43:53 +0900

    리팩토링 - 홈에서 퀘스트 완료 가능

--- app/src/main/java/com/example/capstone4_1/fragment/QuestlistviewFragment.kt
+++ app/src/main/java/com/example/capstone4_1/fragment/QuestlistviewFragment.kt
@@ -1,13 +1,15 @@
 package com.example.capstone4_1.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.capstone4_1.QuestAdapter
-import com.example.capstone4_1.R
+import com.example.capstone4_1.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -42,6 +44,39 @@ class QuestlistviewFragment : Fragment() {
         val rootView = view.findViewById<ListView>(R.id.questListView)
 
         rootView.adapter = QuestAdapter(requireContext())
+
+        rootView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val dialog = AlertDialog.Builder(requireContext())
+                val selectQuest = parent.getItemAtPosition(position) as Quest
+                val questAdapter = QuestAdapter(requireContext())
+
+                //다이얼로그 이름
+                dialog.setTitle(selectQuest.name)
+                //다이얼로그 설명
+                dialog.setMessage(" 이 퀘스트를 해치우셨나용? ")
+
+                // 확인 버튼 클릭시 동작할 것들!!!
+                dialog.setPositiveButton("완료") { dialogInterface, i ->
+                    Toast.makeText(
+                        requireContext(), selectQuest.name + "\n 퀘스트를 완료하셨습니다.", Toast.LENGTH_SHORT
+                    ).show()
+
+                    if (Character.questList[position].value == -1) {
+                        val realPosition = position - Character.randomQuestList.count()
+                        Character.customQuestList.removeAt(realPosition)
+                    } else {
+                        Statistics.addCount(Character.randomQuestList[position])
+                        Character.randomQuestList.removeAt(position)
+                        Character.doingQuestCount++
+                    }
+                    rootView.adapter = QuestAdapter(requireContext())
+                    questAdapter.notifyDataSetChanged()
+                    //퀘스트 수행 완료 시 증가
+                }
+                dialog.setNegativeButton("취소") { dialogInterface, i -> }
+                dialog.show()
+            }
 
         return view
     }
